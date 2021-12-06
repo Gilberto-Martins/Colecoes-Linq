6b366f1582004cdf4a1b1cc0813f352390e095c9
Gilberto Martins <gilbertomartins643@gmail.com>
2021-12-06 17:42:34 -0300

    Arrays

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -6,7 +6,44 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matriz = new int[4, 2]
+            {
+                {8, 8},
+                {10, 20},
+                {50, 100},
+                {90, 200}
+            };
+
+            //linhas
+            for(int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for(int a = 0; a <matriz.GetLength(1); a++)
+                {
+                    Console.WriteLine(matriz[i, a]);
+                }
+            }
+
+
+
+            /*
+            int[] ArrayInteiros = new int[3];
+
+            ArrayInteiros[0] = 10;
+            ArrayInteiros[1] = 20;
+            ArrayInteiros[2] = 30;
+
+            Console.WriteLine("For");
+            for(int i = 0; i < ArrayInteiros.Length; i++)
+            {
+                Console.WriteLine(ArrayInteiros[i]);
+            }
+
+            Console.WriteLine("Foreach");
+            foreach (int item in ArrayInteiros)
+            {
+                Console.WriteLine(item);
+            }
+            */
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Colecoes-Linq
 Fundamentos de Coleções e Linq com .NET
+
+## 
+
+# Arrays e suas Manipulações
+
+Array é uma estrutura de dados que armazena valores do mesmo tipo, com um tamanho fixo. <br>
+
+Pode ser declarado um Array de 3 formas: <br>
+<strong>1. int[] array = new int [4];</strong> <br>
+<strong>2. int[] array = new int []{42,75,74,61};</strong><br>
+<strong>3. string[] nomes = {"Jan","Fev"};</strong><br>
+
+<h3>Disposição do Array</h3> <br>
+Índice: éa posição de um determinado valor de um array, sempre começando com zero. <br>
+
+int[] array = new int []{42,75,74,61} <br>
+var elemento = array[0] <br>
+array[0] = 42 <br>
+
+## 
+
+# Coleções genéricas
+
+##
+
+# Coleções específicas
+
+##
+
+# Dicionários
+
+##
+
+# Operações LINQ
